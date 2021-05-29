719f51101fcc3a218768970433f67deb168e7f24
Zbox09 <84998539+Zbox09@users.noreply.github.com>
2021-05-29 11:08:25 -0400

    Delete test-site directory

--- test-site/scripts/main.js
+++
@@ -1,8 +0,0 @@
-let myImage = document.querySelector('img');
-myImage.onclick = function() {let mySrc = myImage.getAttribute('src');if(mySrc === 'images/p5r.png') {myImage.setAttribute('src','images/applejuice.png');} else {myImage.setAttribute('src','images/p5r.png');}}
-let myButton = document.querySelector('button'); 
-let myHeading = document.querySelector('h1');
-function setUserName() {let myName = prompt('Please enter your name.'); if(!myName) {setUserName();} else {localStorage.setItem('name', myName); myHeading.textContent = 'Maruki is cool, ' + myName;}}
-if(!localStorage.getItem('name')) {setUserName();} else {let storedName = localStorage.getItem('name'); myHeading.textContent = 'Maruki is cool, ' + storedName;}
-//! = logical NOT// 
-myButton.onclick = function() {setUserName();}
